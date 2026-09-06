73c9089d53cba13cf632c9156313b9da346595ac
kamilczynski <kamilczynski1313@gmail.com>
2026-09-06 07:15:39 +0200

    docker configuration.py

--- docker configuration.py
+++ docker configuration.py
@@ -2,12 +2,12 @@
 
 sudo apt update
 sudo apt install -y ca-certificates curl
-
+#############################################
 sudo install -m 0755 -d /etc/apt/keyrings
-
+#############################################
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg \
   -o /etc/apt/keyrings/docker.asc
-
+#############################################
 sudo chmod a+r /etc/apt/keyrings/docker.asc
 #############################################
 sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
@@ -20,16 +20,13 @@ Signed-By: /etc/apt/keyrings/docker.asc
 EOF
 #############################################
 sudo apt update
-
+#############################################
 sudo apt install -y \
   docker-ce \
   docker-ce-cli \
   containerd.io \
   docker-buildx-plugin \
   docker-compose-plugin
-#############################################
-curl -fsSL https://get.docker.com -o /tmp/get-docker.sh
-sudo sh /tmp/get-docker.sh
 #############################################
 sudo usermod -aG docker $USER
 newgrp docker
